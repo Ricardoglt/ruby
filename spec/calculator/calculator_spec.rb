63c827b9b73fa94ad049b50eae89dc12e0a832b9
Carlos Tiago <ontiago@gmail.com>
2022-01-27 21:16:52 -0300

    Atualiza testes para subtração

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -4,20 +4,47 @@ require 'calculator'
 
 describe Calculator do
   subject(:calc) { described_class.new }
-  context '#sum' do
+  context 'somar' do
     it 'com números positivos' do
-      result = calc.sum(5, 7)
-      expect(result).to eq(12)
+      result = calc.somar(3,2)
+      expect(result).to eq(5)
     end
 
     it 'com números positivos e negativos' do
-        result = calc.sum(-5, 7)
+      result = calc.somar(-5, 7)
       expect(result).to eq(2)
     end
 
     it 'com números negativos' do
-      result = calc.sum(-5, -7)
+      result = calc.somar(-5, -7)
       expect(result).to eq(-12)
+    end
+
+    it 'com números reais' do
+      result = calc.somar(1.2, 1)
+      expect(result).to eq(2.2)
+    end
+  end
+
+  context 'subtração' do
+    it 'com números positivos' do
+      result = calc.subtrair(3,2)
+      expect(result).to eq(1)
+    end
+
+    it 'com números positivos e negativos' do
+      result = calc.subtrair(-5, 7)
+      expect(result).to eq(12)
+    end
+
+    it 'com números negativos' do
+      result = calc.subtrair(-5, -7)
+      expect(result).to eq(2)
+    end
+
+    it 'com números reais' do
+      result = calc.subtrair(1.2, 1)
+      expect(result).to eq(0.1)
     end
   end
 end
